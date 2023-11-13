7176a55989fa094956fdadac5efe5e132cfe98a3
Kiranuday97 <117077183+Kiranuday97@users.noreply.github.com>
2023-11-13 16:25:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 databaseConfiguration();
 
-app.use(cors({
-  origin: ["http://localhost:3000", "http://megaminds-project.onrender.com"],
-}
-));
+app.use(cors({ origin: 'https://megaminds-project.onrender.com' }));
 
 
 app.use(express.json({ extended: false }));
